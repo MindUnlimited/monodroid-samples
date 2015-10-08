1ee534e13c7a1c462a799f34acdaaae2c6d645de
Rob Klein Hofmeijer <rob@mordomo.nl>
2015-10-08 11:54:03 +0200

    Did a lot of styling, started work on selecting contacts to share with/assign task to

--- android5.0/Cheesesquare/EditItemActivity.cs
+++ android5.0/Cheesesquare/EditItemActivity.cs
@@ -10,6 +10,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Support.V7.App;
+using Android.Provider;
+using Android.Util;
 
 namespace Cheesesquare
 {
@@ -17,7 +19,10 @@ namespace Cheesesquare
     public class EditItemActivity : AppCompatActivity
     {
         private Android.Support.V7.Widget.Toolbar toolbar;
+        private EditText shareEditText;
+        EditText txtDate;
         public const string EXTRA_NAME = "cheese_name";
+        private readonly int PICK_CONTACT = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -31,13 +36,77 @@ namespace Cheesesquare
             toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.appbar_edit);
             toolbar.Title = cheeseName;
             SetSupportActionBar(toolbar);
+
+            
+
         }
 
         protected override void OnStart()
         {
             base.OnStart();
-            EditText txtDate = (EditText)FindViewById(Resource.Id.item_date);
+            txtDate = (EditText)FindViewById(Resource.Id.item_date);
             txtDate.FocusChange += TxtDate_FocusChange;
+
+            shareEditText = FindViewById<EditText>(Resource.Id.add_user_text);
+            shareEditText.FocusChange += ShareEditText_FocusChange;
+        }
+
+        private void ShareEditText_FocusChange(object sender, View.FocusChangeEventArgs e)
+        {
+            var editText = (EditText)sender;
+            if (e.HasFocus)
+            {
+                Intent intent = new Intent(Intent.ActionPick, ContactsContract.Contacts.ContentUri);
+                StartActivityForResult(intent, PICK_CONTACT);//PICK_CONTACT is private static final int, so declare in activity class
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode,
+        Intent intent)
+        {
+            if (requestCode == PICK_CONTACT)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    //Android.Net.Uri contactData = intent.Data;
+                    //Cursor c = ManagedQuery(contactData, null, null, null, null);
+                    //if (c.moveToFirst())
+                    //{
+
+
+                    //    String id = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+
+                    //    String hasPhone = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                    //    if (hasPhone.equalsIgnoreCase("1"))
+                    //    {
+                    //        Cursor phones = getContentResolver().query(
+                    //                     ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                    //                     ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,
+                    //                     null, null);
+                    //        phones.moveToFirst();
+                    //        cNumber = phones.getString(phones.getColumnIndex("data1"));
+                    //        System.out.println("number is:" + cNumber);
+                    //    }
+                    //    String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    Android.Net.Uri uri = intent.Data;
+                    String[] projection = { ContactsContract.CommonDataKinds.Phone.Number, ContactsContract.CommonDataKinds.StructuredName.DisplayName}; // PROBLEM OVER HERE?
+
+                    var cursor = ContentResolver.Query(uri, projection,
+                            null, null, null);
+                    cursor.MoveToFirst();
+
+                    int numberColumnIndex = cursor.GetColumnIndex(ContactsContract.CommonDataKinds.Phone.Number);
+                    String number = cursor.GetString(numberColumnIndex);
+
+                    int nameColumnIndex = cursor.GetColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DisplayName);
+                    String name = cursor.GetString(nameColumnIndex);
+
+                    Log.Debug(this.LocalClassName, "ZZZ number : " + number + " , name : " + name);
+
+                }
+            }
         }
 
         private void TxtDate_FocusChange(object sender, View.FocusChangeEventArgs e)
